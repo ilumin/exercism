a2ebb9d91e7620a9e678185fdeb0dc788dc547e8
O Teerasak Vichadee <iolumin@gmail.com>
2022-06-28 14:19:03 +0700

    init

---
+++ clojurescript/bob/src/bob.cljs
@@ -0,0 +1,10 @@
+(ns bob
+  (:require [clojure.string :as str]))
+
+(defn response-for 
+    ([] ("Whatever."))
+    ([] ("Fine. Be that way!"))
+    ([] ("Calm down, I know what I'm doing!"))
+    ([] ("Whoa, chill out!"))
+    ([] ("Sure."))
+    )
